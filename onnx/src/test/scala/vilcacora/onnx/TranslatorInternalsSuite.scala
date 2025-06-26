8be966020a1f7d17dc09bd78bdaac03449d883e4
Shrey Pant <pantshrey01@gmail.com>
2025-06-26 18:20:38 +0530

    ran sbt prePR

--- onnx/src/test/scala/vilcacora/onnx/TranslatorInternalsSuite.scala
+++ onnx/src/test/scala/vilcacora/onnx/TranslatorInternalsSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vilcacora.onnx
 
 import com.armanbilge.vilcacora.ir._
@@ -25,15 +41,16 @@ class TranslatorInternalsSuite extends FunSuite {
     val shapeProto = TensorShapeProto(
       dim = Seq(
         TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.DimValue(1)),
-        TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.DimValue(30))
-      )
+        TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.DimValue(30)),
+      ),
     )
     assertEquals(Translator.parseShape(shapeProto), Right(List(1, 30)))
   }
 
   test("parseShape should fail on a dynamic dimension parameter") {
     val shapeProto = TensorShapeProto(
-      dim = Seq(TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.DimParam("unk_0")))
+      dim =
+        Seq(TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.DimParam("unk_0"))),
     )
     val result = Translator.parseShape(shapeProto)
     assert(result.isLeft, "Should fail for dynamic named dimension")
@@ -42,7 +59,9 @@ class TranslatorInternalsSuite extends FunSuite {
 
   test("parseShape should fail on an empty (unknown) dimension") {
     val shapeProto =
-      TensorShapeProto(dim = Seq(TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.Empty)))
+      TensorShapeProto(dim =
+        Seq(TensorShapeProto.Dimension(value = TensorShapeProto.Dimension.Value.Empty)),
+      )
     val result = Translator.parseShape(shapeProto)
     assert(result.isLeft, "Should fail for un-inferred dimension")
     result.left.foreach(err => assert(err.contains("unknown dimension")))
@@ -51,12 +70,16 @@ class TranslatorInternalsSuite extends FunSuite {
   // --- Allocation Creation ---
 
   test("createAllocation should build a valid Allocation from a ValueInfoProto") {
-    val shape = TensorShapeProto(dim = Seq(TensorShapeProto.Dimension(TensorShapeProto.Dimension.Value.DimValue(10))))
-    val typeProto = TypeProto(value = TypeProto.Value.TensorType(TypeProto.Tensor(elemType = 1, shape = Some(shape))))
+    val shape = TensorShapeProto(dim =
+      Seq(TensorShapeProto.Dimension(TensorShapeProto.Dimension.Value.DimValue(10))),
+    )
+    val typeProto = TypeProto(value =
+      TypeProto.Value.TensorType(TypeProto.Tensor(elemType = 1, shape = Some(shape))),
+    )
     val valueInfo = ValueInfoProto(name = "test_tensor", `type` = Some(typeProto))
 
     val result = Translator.createAllocation(valueInfo, None)
-    
+
     val expected = Allocation("test_tensor", DataType.Float32, List(10), None)
     assertEquals(result, Right(expected))
   }
@@ -67,9 +90,9 @@ class TranslatorInternalsSuite extends FunSuite {
       name = "weights",
       dims = Seq(4),
       dataType = 3, // INT8
-      rawData = ByteString.copyFrom(rawBytes)
+      rawData = ByteString.copyFrom(rawBytes),
     )
-    
+
     val result = Translator.createAllocationFromInitializer(tensorProto)
     assert(result.isRight)
     result.foreach { alloc =>
@@ -85,14 +108,15 @@ class TranslatorInternalsSuite extends FunSuite {
   test("extractBytes should prioritize raw_data field") {
     val rawBytes = Array[Byte](0, 0, -128, 63) // 1.0f
     val tensor = TensorProto(dims = Seq(1), dataType = 1, rawData = ByteString.copyFrom(rawBytes))
-    
+
     val result = Translator.extractBytes(tensor, DataType.Float32)
     assert(result.map(_.toSeq) == Right(rawBytes.toSeq))
   }
 
   test("extractBytes should fall back to typed data fields") {
     val tensor = TensorProto(dims = Seq(2), dataType = 1, floatData = Seq(1.0f, -1.0f))
-    val expectedBytes = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putFloat(1.0f).putFloat(-1.0f).array()
+    val expectedBytes =
+      ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putFloat(1.0f).putFloat(-1.0f).array()
 
     val result = Translator.extractBytes(tensor, DataType.Float32)
     assert(result.map(_.toSeq) == Right(expectedBytes.toSeq))
--- onnx/src/test/scala/vilcacora/onnx/TranslatorSuite.scala
+++ onnx/src/test/scala/vilcacora/onnx/TranslatorSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vilcacora.onnx
 
 import com.armanbilge.vilcacora.ir.{DataType, Operation, PostTransform, SVMKernel}
@@ -14,7 +30,7 @@ class TranslatorSuite extends FunSuite {
     val node = NodeProto(opType = "MatMul", input = Seq("A", "B"), output = Seq("C"))
     assertEquals(Translator.translateNode(node), Right(Operation.MatMul("A", "B", "C")))
   }
-  
+
   test("translateNode should translate an Add node") {
     val node = NodeProto(opType = "Add", input = Seq("X", "Y"), output = Seq("Z"))
     assertEquals(Translator.translateNode(node), Right(Operation.Add("X", "Y", "Z")))
@@ -25,13 +41,17 @@ class TranslatorSuite extends FunSuite {
       opType = "Cast",
       input = Seq("in"),
       output = Seq("out"),
-      attribute = Seq(AttributeProto(name = "to", i = 6, `type` = AttributeProto.AttributeType.INT)) // 6 = INT64
+      attribute = Seq(
+        AttributeProto(name = "to", i = 6, `type` = AttributeProto.AttributeType.INT),
+      ), // 6 = INT64
     )
     assertEquals(Translator.translateNode(node), Right(Operation.Cast("in", "out", DataType.Int64)))
   }
 
   // --- REWRITTEN TEST FOR SVMCLASSIFIER ---
-  test("translateNode should correctly translate an SVMClassifier node with floating point tolerance") {
+  test(
+    "translateNode should correctly translate an SVMClassifier node with floating point tolerance",
+  ) {
     val node = NodeProto(
       opType = "SVMClassifier",
       input = Seq("features"),
@@ -44,27 +64,27 @@ class TranslatorSuite extends FunSuite {
         AttributeProto(name = "post_transform", s = ByteString.copyFromUtf8("SOFTMAX")),
         AttributeProto(name = "rho", floats = Seq(0.9f)),
         AttributeProto(name = "support_vectors", floats = Seq(1.1f, 2.2f)),
-        AttributeProto(name = "vectors_per_class", ints = Seq(1L, 1L))
-      )
+        AttributeProto(name = "vectors_per_class", ints = Seq(1L, 1L)),
+      ),
     )
 
     val result = Translator.translateNode(node)
     assert(result.isRight, "SVMClassifier translation failed unexpectedly")
-    
+
     // Define a small tolerance for comparing floating point numbers
-    val epsilon = 1e-6 
+    val epsilon = 1e-6
 
     result.foreach {
       case op: Operation.SVMClassifier =>
         assertEquals(op.classLabels, List(0L, 1L))
         assertEquals(op.kernelType, SVMKernel.Rbf)
         assertEquals(op.postTransform, PostTransform.Softmax)
-        
+
         // Use assertEquals with a tolerance (epsilon) for doubles
         assertEqualsDouble(op.kernelParams.head, 0.5, epsilon)
         assertEqualsDouble(op.kernelParams(1), 0.1, epsilon)
         assertEqualsDouble(op.rho.head, 0.9, epsilon)
-        
+
         // Compare arrays of doubles element-by-element with tolerance
         op.coefficients.zip(Array(1.5, -1.5)).foreach { case (actual, expected) =>
           assertEqualsDouble(actual, expected, epsilon)
@@ -76,9 +96,7 @@ class TranslatorSuite extends FunSuite {
     }
   }
 
-
   // --- End-to-end tests using model files from resources ---
-
 
   // Helper now includes the leading slash, which is the correct way to specify an absolute path on the classpath
   def loadModelFromPath(modelPath: String): ModelProto = {
@@ -89,7 +107,7 @@ class TranslatorSuite extends FunSuite {
   test("translate should successfully process a valid, fully static model") {
     // This test now depends on the file 'static_svm.onnx' being in 'onnx/src/test/resources'
     val modelProto = loadModelFromPath("onnx/src/test/resources/static_svm.onnx")
-    val irResult   = Translator.translate(modelProto)
+    val irResult = Translator.translate(modelProto)
 
     assert(irResult.isRight, "Translation failed unexpectedly for a valid model")
     irResult.foreach { modelIR =>
@@ -101,7 +119,7 @@ class TranslatorSuite extends FunSuite {
   test("translate should fail gracefully for a model with dynamic shapes") {
     // This test now depends on the file 'dynamic_svm.onnx' being in 'onnx/src/test/resources'
     val modelProto = loadModelFromPath("onnx/src/test/resources/dynamic_svm.onnx")
-    val irResult   = Translator.translate(modelProto)
+    val irResult = Translator.translate(modelProto)
 
     assert(irResult.isLeft, "Translation should have failed but it succeeded")
     irResult.left.foreach { error =>
